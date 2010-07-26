190f7a956ddc384c1b2a0e52e0f62504ad7c6dd3
broli <carlos.ferrabone@gmail.com>
2010-07-26 15:41:51 -0300

    Initial Commit

---
+++ src/SpellManager.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author carlos
+ *
+ */
+public class SpellManager {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
