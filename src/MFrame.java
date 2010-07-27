2ec4465a858a55e25850e6b8475c9ec4bea2dc4f
broli <carlos.ferrabone@gmail.com>
2010-07-26 22:36:22 -0300

    Its looking better Still alot to do

--- src/MFrame.java
+++ src/MFrame.java
@@ -1,8 +1,8 @@
-import java.awt.GridLayout;
+import java.awt.Dimension;
 
-import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -31,6 +31,9 @@ import javax.swing.JTextField;
  *
  */
 public class MFrame {
+	
+	String[] SClases = { "Bard", "Cleric", "Druid", "Paladin", "Ranger", "Sorcerer", "Wizard"};
+	String[] SLevels = { "0", "1","2","3","4","5","6","7","8","9"};
 
 	public void goMain() {
 		JFrame MainJFrame = new JFrame("SpellManager");
@@ -41,15 +44,23 @@ public class MFrame {
 		
 		//Tier 1 (first line)
 		JPanel JPTier1 = new JPanel();
-		//JPTier1.setLayout(new BoxLayout(JPTier1,BoxLayout.X_AXIS));
-		JPTier1.setLayout(new GridLayout(1, 3));
+		JPTier1.setMaximumSize(new Dimension(500,50));
+		
 		
 		//spell name
 		JLabel JLSpellName = new JLabel("Spell Name");
-		JTextField JTFSpellName = new JTextField(10);
-		JPTier1.add(JLSpellName);  
+		JTextField JTFSpellName = new JTextField(25);
+		JPTier1.add(JLSpellName);
 		JPTier1.add(JTFSpellName);
-
+		
+		//Class and level selector
+		JPanel JPTier2 = new JPanel();
+		JPTier2.setLayout(new BoxLayout(JPTier2,BoxLayout.X_AXIS));
+		JPTier2.setMaximumSize(new Dimension(500,50));
+		JComboBox JComboClass = new JComboBox(SClases);
+		JComboBox JComboLevel = new JComboBox(SLevels);
+		JPTier2.add(JComboClass);
+		JPTier2.add(JComboLevel);
 		
 		//Components of spells
 		JPanel JPComponents = new JPanel();
@@ -57,11 +68,15 @@ public class MFrame {
 		JCheckBox JCBVerbal = new JCheckBox("Verbal");
 		JCheckBox JCBSomatic = new JCheckBox("Somatic");
 		JCheckBox JCBMaterial = new JCheckBox("Material");
+		JCheckBox JCBFocus = new JCheckBox("Focus");
+		JCheckBox JCBDivine_Focus = new JCheckBox("Divine Focus");
+		JCheckBox JCBXP_Cost = new JCheckBox("XP Cost");
 		JPComponents.add(JCBVerbal);
 		JPComponents.add(JCBSomatic);
 		JPComponents.add(JCBMaterial);
-		JPTier1.add(JPComponents);
-		
+		JPComponents.add(JCBFocus);
+		JPComponents.add(JCBDivine_Focus);
+		JPComponents.add(JCBXP_Cost);
 		
 		//Main description area, with scroller
 		JTextArea JTADescription = new JTextArea(10,100);
@@ -71,6 +86,7 @@ public class MFrame {
 		
 		//Adding everyting to the main panel
 		MainJPanel.add(JPTier1);
+		MainJPanel.add(JPTier2);
 		MainJPanel.add(JPComponents);
 		MainJPanel.add(JSPTADescription);
 		
