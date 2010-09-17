46aaa34b22fd478c0f236bd82ffd086402191f32
broli <carlos.ferrabone@gmail.com>
2010-09-17 16:52:15 -0300

    Wraper class to manage all DB transactions

---
+++ src/com/ferrabone/spellmanager/SpellDBWraper.java
@@ -0,0 +1,24 @@
+/* This file is part of SpellManager
+*
+* SpellManager is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* SpellManager is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with SpellManager. If not, see <http://www.gnu.org/licenses/>.
+*/
+package com.ferrabone.spellmanager;
+
+/**
+ * @author carlos
+ *
+ */
+public class SpellDBWraper {
+
+}
