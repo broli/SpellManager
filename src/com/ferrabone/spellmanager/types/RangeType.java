8621503c8ceb4a3abce7cd1a14ae405867952c73
broli <carlos.ferrabone@gmail.com>
2010-09-21 10:55:42 -0300

    new helping classes to hold part of a spell, and info from the DB

---
+++ src/com/ferrabone/spellmanager/types/RangeType.java
@@ -0,0 +1,62 @@
+/* This file is part of SpellManager
+*
+* SpellManager is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* SpellManager is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with SpellManager. If not, see <http://www.gnu.org/licenses/>.
+*/
+package com.ferrabone.spellmanager.types;
+
+/**
+ * @author cferrabo
+ *
+ */
+public class RangeType extends IDStringPairType {
+	private int range=0;
+	
+	/**
+	 * Creates an obj with empty members
+	 */
+	public RangeType(){
+		// this automaticly calls super with no args
+	}
+	/**
+	 * 
+	 * @param Range to use (other members are created empty)
+	 */
+	public RangeType(int Range){
+		super();
+		this.setRange(Range);
+	}
+	/**
+	 * Fully parameterized constructor
+	 * @param iD
+	 * @param name
+	 * @param range
+	 */
+	public RangeType(int iD,String name,int range){
+		super(iD,name);
+		this.setRange(range);
+	}
+	/**
+	 * @param range the range to set
+	 */
+	public void setRange(int range) {
+		this.range = range;
+	}
+	/**
+	 * @return the range
+	 */
+	public int getRange() {
+		return range;
+	}
+
+}
