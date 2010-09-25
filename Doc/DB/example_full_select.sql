aab1d4ebe063458b7617bc2f512027b084b5f2e7
broli <carlos.ferrabone@gmail.com>
2010-09-25 20:02:28 -0300

    Trying to get the SELECT statement working

--- Doc/DB/example_full_select.sql
+++ Doc/DB/example_full_select.sql
@@ -1,4 +1,4 @@
 ﻿
-SELECT a.spell_id, spell_name,
-	FROM spells AS a,school_info 
-WHERE spells.spell_id = 1;
+SELECT a.spell_id, spell_name, b.time,c.range_name,c.rage_distance, target, d.duration, e.save, f.resistance_text, effect, text_id
+	FROM spells AS a,castingTime AS b, range AS c, duration AS d, savingThrow AS e, resistance AS f
+WHERE a.spell_id = 1;
