0ab7f67c8900e1dc6b3b57c4d4f42e58fd512839
broli <carlos.ferrabone@gmail.com>
2010-09-27 16:36:16 -0300

    forgot to get all the DB ids

--- Doc/DB/example_full_select.sql
+++ Doc/DB/example_full_select.sql
@@ -1,7 +1,7 @@
 --- first the main body with all the 1to1 tables
-SELECT a.spell_id, a.spell_name, g.school_name, g.subschool_name, b.time,c.range_name,c.rage_distance, a.target, d.duration, e.save, f.resistance_text, a.effect, a.text_id
-	FROM spells AS a NATURAL JOIN castingTime AS b NATURAL JOIN range AS c NATURAL JOIN duration AS d NATURAL JOIN savingThrow AS e NATURAL JOIN resistance AS f NATURAL JOIN (SELECT * FROM school_info NATURAL JOIN schools NATURAL JOIN subschools) as g
-WHERE a.spell_id = 1;
+SELECT *
+	FROM spells NATURAL JOIN castingTime NATURAL JOIN range NATURAL JOIN duration NATURAL JOIN savingThrow NATURAL JOIN resistance NATURAL JOIN (SELECT * FROM school_info NATURAL JOIN schools NATURAL JOIN subschools)
+WHERE spells.spell_id = 1;
 
 --- Now, one by one, to select the 1toMany tables
 
