a849274ba4fa1775de6a4d4d867c679b03b8e42c
broli <carlos.ferrabone@gmail.com>
2010-09-27 16:15:18 -0300

    ifinished the SQL statements to get all data

--- Doc/DB/example_full_select.sql
+++ Doc/DB/example_full_select.sql
@@ -1,4 +1,21 @@
-﻿
-SELECT a.spell_id, spell_name, b.time,c.range_name,c.rage_distance, target, d.duration, e.save, f.resistance_text, effect, text_id
-	FROM spells AS a,castingTime AS b, range AS c, duration AS d, savingThrow AS e, resistance AS f
+--- first the main body with all the 1to1 tables
+SELECT a.spell_id, a.spell_name, g.school_name, g.subschool_name, b.time,c.range_name,c.rage_distance, a.target, d.duration, e.save, f.resistance_text, a.effect, a.text_id
+	FROM spells AS a NATURAL JOIN castingTime AS b NATURAL JOIN range AS c NATURAL JOIN duration AS d NATURAL JOIN savingThrow AS e NATURAL JOIN resistance AS f NATURAL JOIN (SELECT * FROM school_info NATURAL JOIN schools NATURAL JOIN subschools) as g
 WHERE a.spell_id = 1;
+
+--- Now, one by one, to select the 1toMany tables
+
+--- First, the descriptors
+SELECT * FROM descriptor_info NATURAL JOIN descriptors WHERE spell_id = 1;
+
+--- Caster's class and level
+SELECT * FROM class_info NATURAL JOIN casters WHERE spell_id = 1;
+
+--- Components
+SELECT * FROM components_info NATURAL JOIN components WHERE spell_id = 1;
+
+--- Components
+SELECT * FROM components_info NATURAL JOIN components WHERE spell_id = 1;
+
+--- Domains
+SELECT * FROM domain_info NATURAL JOIN domains WHERE spell_id = 1;
